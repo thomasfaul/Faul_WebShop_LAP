8962c1f7a124f4ba5d673e4a12ec25e750a0c639
thomasfaul <thomas.faul@gmx.at>
2017-04-04 02:17:40 +0200

    Design

---
+++ CardGame/CardGame/CardGame.Web/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardGame.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        ActionResult a;
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return a;
+        }
+    }
+}
