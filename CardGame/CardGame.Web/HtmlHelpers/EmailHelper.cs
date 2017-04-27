cf1ad15e5832a465724caf693a598f7926c25aae
thomasfaul <thomas.faul@gmx.at>
2017-04-27 09:21:02 +0200

    neu eingespielt die 500tste

---
+++ CardGame/CardGame.Web/HtmlHelpers/EmailHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace CardGame.Web.HtmlHelpers
+{
+    public class EmailHelper
+    {
+        #region SEND EMAIL
+        public static bool SendEmail(string emailaddress, string emailsubject, string text)
+        {
+
+            #region EMAILBBRZ
+            MailMessage Email = new MailMessage();
+            MailAddress Sender = new MailAddress("Thomas.Faul@qualifizierung.or.at");
+            Email.From = Sender;
+            Email.To.Add(emailaddress);
+            Email.Subject = emailsubject;
+
+            Email.Body = text;
+
+            string ServerName = "srv08.itccn.loc";
+            string Port = "25";
+
+
+            SmtpClient MailClient = new SmtpClient(ServerName, int.Parse(Port)); // Postausgangsserver definieren
+
+            string UserName = "thomas.faul@gmx.at";
+
+            string Password = "tom362461!";
+            System.Net.NetworkCredential Credentials = new System.Net.NetworkCredential(UserName, Password);
+            MailClient.Credentials = Credentials; // Anmeldeinformationen setzen*/
+            MailClient.Send(Email); // Email senden
+            return true;
+            #endregion
+
+            #region EMAIL GMX
+        
+            //MailMessage Email = new MailMessage();
+            //MailAddress Sender = new MailAddress("thomas.faul@gmx.at");
+
+            //Email.From = Sender;
+            //Email.To.Add(emailaddress);
+            //Email.Subject = emailsubject;
+
+            //Email.Body = text;
+
+            //string ServerName = "mail.gmx.net";
+            //string Port = "587";
+
+
+
+            //SmtpClient MailClient = new SmtpClient(ServerName, int.Parse(Port)); // Postausgangsserver definieren
+
+            //string UserName = "thomas.faul@gmx.at";
+
+            ////string Password ="tom362461gmx";
+            //string Password = "tom362461!";
+            //System.Net.NetworkCredential Credentials = new System.Net.NetworkCredential(UserName, Password);
+            //MailClient.Credentials = Credentials; // Anmeldeinformationen setzen*/
+            //MailClient.Send(Email); // Email senden
+            //return true;
+
+            #endregion
+        }
+#endregion
+    }
+}
